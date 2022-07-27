30f79a0d456315a218ebcd54eb77c5890722a6b1
= <abhidnya09@gmail.com>
2022-07-27 16:46:27 +0530

    linked List modified

--- src/com/JavaDSA/Linklist.java
+++ src/com/JavaDSA/Linklist.java
@@ -1,23 +1,35 @@
 package com.JavaDSA;
 
-public class Linklist {
-    Node head;
-    public class Node{
-        String data;
-        Node next ;
+public class Linklist{
 
-        Node(String data){
-            this.data = data;
-            this.next = null;
-        }
-        }
-        public void addfirst(String data){
-        Node newnode = new Node(data);
-        if(head == null){
-            newnode.next = head;
-            head = newnode;
-        }
-        }
-    public static void main(String[] args) {
+}
+
+
+class Node{
+    private int data;
+    private Node next;
+    Node(){
+        data = 0;
+        next = null;
+    }
+    Node(int d,Node n){
+        data = d;
+        next = n;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
